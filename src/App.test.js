aa62723be9016b115cc1cd62d80682f974cbc858
Stanislav Kosatkin <stas.kosatkin@gmail.com>
2020-07-28 22:16:29 +0300

    init commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hi there!!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hi there!!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hi there!!/i);
   expect(linkElement).toBeInTheDocument();
 });
